50bbd2c9abed5ab92c5ceffc0692afbd21c1b54a
Dwatson783 <dwatson783@gmail.com>
2011-05-22 22:20:46 -0400

    Added password and encrypted_password to the model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com"}
+    @attr = { :name => "Example User", 
+              :email => "user@example.com",
+              :password => "foobar",
+              :password_confirmation => "foobar"}
   end
   
   it "should create a new instance given a valid attribute" do
@@ -55,5 +58,51 @@ describe User do
     upcased_email_user = User.new(@attr)
     upcased_email_user.should_not be_valid
   end
-
+  
+  describe "passwords" do 
+    before(:each) do
+      @user = User.new(@attr)
+    end
+    
+    it "should have a password attribute" do
+      @user.should respond_to(:password)
+    end
+    
+    it "should have a password confirmation attribute" do
+      @user.should respond_to(:password_confirmation)
+    end    
+  end
+  
+  describe "password validation" do
+    it "should require a password" do
+      User.new(@attr.merge(:password =>"", :password_confirmation =>"")).
+        should_not be_valid
+    end
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+      should_not be_valid
+    end
+    it "should reject short passwords" do
+      short_password = "a"*5
+      hash = @attr.merge(:password => short_password, :password_confirmation => short_password)
+      User.new(hash).should_not be_valid
+    end
+    it "should reject long passwords" do
+      long_password = "a"*41
+      hash = @attr.merge(:password => long_password)
+      User.new(hash).should_not be_valid
+    end
+  end
+  
+  describe "password encryption" do
+    
+    before (:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrpyted password element" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
+  
 end
