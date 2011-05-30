d963a3eb13763b5cb989cbed37a459be25dfb23a
Dwatson783 <dwatson783@gmail.com>
2011-05-30 15:18:24 -0400

    Finished the password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,37 @@ class User < ActiveRecord::Base
   validates :password, :presence => true,
                        :confirmation => true,
                        :length => {:within => 6..40}
+  
+  before_save :encrypt_password 
+  
+  def has_password?(submitted_password)
+    encrpyted_password == encrpyt(submitted_password)
+  end  
+  
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private 
+  
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+    
+    def encrypt(string)
+      secure_hash( "#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def salt(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
+
+
